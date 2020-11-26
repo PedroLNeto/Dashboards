e1f59aee6b6a76a7f4ffeb7e442606c72ae34033
PedroLNeto <62525418+PedroLNeto@users.noreply.github.com>
2020-11-26 11:42:42 -0300

    Inicialização do repositorio

---
+++ dashboard/conexao.php
@@ -0,0 +1,12 @@
+<?php
+$servername= "localhost";
+$database = "baseteste";
+$username =  "root";
+$password = "";
+
+//create a connection
+
+$conexao = mysqli_connect($servername,$username,$password,$database);
+
+?>
+
